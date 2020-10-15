d1c3a6c385f4c60bb6793c69b8105c05c5b1cd84
Dr Phill Edwards <phill@aux-quatre-vents-france.fr>
2020-10-15 16:47:57 +0200

    Added messaging loop to client

--- client/control.js
+++ client/control.js
@@ -15,3 +15,20 @@ function onlogin() {
         alert("Error " + error);
     });
 }
+
+var timer = null;
+var counter = 1;
+function ontimer() {
+    var button = document.getElementById('timer');
+    if (button.value == 'Start') {
+        document.getElementById('messages').innerHTML = 'Messages Poo\n';
+        button.value = 'Stop';
+        timer = setInterval(function() {
+            document.getElementById('messages').innerHTML += 'Message ' + counter++ + '\n';
+        }, 2000);
+    } else {
+        clearInterval(timer);
+        button.value = 'Start';
+        counter = 1;
+    }
+}
